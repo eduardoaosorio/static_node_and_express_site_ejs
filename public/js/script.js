6b8b878383a991dd1bf58499c51e8fc7cb8c1421
Eduardo Osorio <eduardoaosorio95@gmail.com>
2021-03-30 12:40:04 -0500

    Hide about link when in about page

--- public/js/script.js
+++ public/js/script.js
@@ -1,13 +1,24 @@
-
-'use strict';
+"use strict";
 
 /**
  * Handle mobile menu functionality to hide/reveal sidebar on mobile layouts
  */
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 let headerBtnClicked = false;
 
-document.querySelector('#menu-icon').addEventListener('click', e => {
-  !headerBtnClicked ? body.style.transform = 'translateX(300px)' : body.style.transform = 'translateX(0px)';
-  return headerBtnClicked = !headerBtnClicked;
+document.querySelector("#menu-icon").addEventListener("click", (e) => {
+  !headerBtnClicked
+    ? (body.style.transform = "translateX(300px)")
+    : (body.style.transform = "translateX(0px)");
+  return (headerBtnClicked = !headerBtnClicked);
 });
+
+// select about link
+const aboutLink = document.querySelector("#about-link");
+
+// hide about link if in about page, else display
+if (document.querySelector(".portfolio-about")) {
+  aboutLink.style.display = "none";
+} else {
+  aboutLink.style.display = "";
+}
